63a307492fdf71f5b3b5552eb46f6080950cb6a7
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-12-12 09:32:10 +0000

    Updated script.

--- deploy-if.sh
+++ deploy-if.sh
@@ -3,9 +3,13 @@ if [ $? -ne 0 ]; then
   exit -1
 fi
 
+# Logic for dealing with pull requests:
 if [ -n $TRAVIS_PULL_REQUEST ]; then
-if [ $TRAVIS_PULL_REQUEST -eg 'false' ]; then
-  mvn deploy --settings target/travis/settings.xml -B
+echo "TRAVIS_PULL_REQUEST is set to" $TRAVIS_PULL_REQUEST
+
+if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
+  echo "TRAVIS_PULL_REQUEST is not false. Deploying..."
+  mvn deploy --settings target/travis/settings.xml -B -DskipTests
 if [ $? -ne 0 ]; then
   exit -1
 fi
