c324d4142b8d75a9e2f59504fe9ebe5ea079d1c5
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-12-12 09:06:37 +0000

    Ensure the fail is passed back.

--- deploy-if.sh
+++ deploy-if.sh
@@ -1,6 +1,14 @@
 mvn install --settings target/travis/settings.xml -B
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
 if [ -n $TRAVIS_PULL_REQUEST ]; then
 if [ $TRAVIS_PULL_REQUEST == 'true' ]; then
   mvn deploy --settings target/travis/settings.xml -B
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
 fi
 fi
