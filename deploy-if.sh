f74742a9f5c950cc97a03a845589f9479e4c9844
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-12-12 11:22:36 +0000

    Fixed script so failed deployments are not fatal.

--- deploy-if.sh
+++ deploy-if.sh
@@ -3,16 +3,18 @@ if [ $? -ne 0 ]; then
   exit -1
 fi
 
-# Logic for dealing with pull requests:
+#
+# Attempt to deploy SNAPSHOTS.
+#
+# We deliberatly ignore failed snapshot deployments, as this can happen during the release procedure.
+#
+# Logic for ignoring pull requests:
 if [ $TRAVIS_SECURE_ENV_VARS == 'true' ]; then
 echo "TRAVIS_SECURE_ENV_VARS is true, attempting deployment..."
 
 if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   echo "TRAVIS_PULL_REQUEST is false. Deploying..."
   mvn deploy --settings target/travis/settings.xml -B -DskipTests
-if [ $? -ne 0 ]; then
-  exit -1
-fi
 
 fi
 fi
